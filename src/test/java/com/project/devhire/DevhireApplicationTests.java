6277d6905b82e2e9f20193a57c4007269dcef539
tony <tony950620@naver.com>
2021-05-25 09:00:18 +0900

    init commit

---
+++ src/test/java/com/project/devhire/DevhireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.devhire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevhireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
